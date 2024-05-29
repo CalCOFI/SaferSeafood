4f52227a30340bebd114086d7149223226b52f8a
BenVerst <Bjwversteeg@gmail.com>
2024-05-29 14:46:51 -0700

    ui changes

--- shinydashboard/ui.R
+++ shinydashboard/ui.R
@@ -71,13 +71,11 @@ body <-dashboardBody(
                              class = "map-container",
                              HTML("<div style='text-align: center;'><span style='font-size: 20px;'>Caught a fish off the coast of Southern California? Fill in the required fields below to understand the DDT levels in your seafood and receive serving size recommendations based on DDT, Mercury, and PCB advisories.</span></div>"),
                              HTML("<br><br>"), 
-                             tags$b("Step 1: Click the location on the map where your fish was caught within the study area (outlined in blue)", 
-                                    style = "color: #FFDB00; font-size: 20px; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;"),
-                             
+                             tags$b("Step 1: Click the location on the map where your fish was caught within the study area (outlined in blue)", style = "color:#f2570f; font-size: 20px;"),
                              HTML("<br><br>"), 
                              
                              tags$div(tags$b("Step 2: Please select your catch species"), 
-                                      style = "font-size: 20px; color:#3b9a58;"),
+                                      style = "font-size: 20px; color:#f2570f;"),
                              
                              HTML("<br><br>"), 
                              HTML("<div style='text-align: center;'><span style='font-size: 16px;'>Click through map layers to gather more information</span></div>"),
